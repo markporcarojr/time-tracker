7748a2d1c4b6a84b72c83746489a58fa63f4fb34
Mark <markporcarojr@gmail.com>
2025-08-11 13:18:55 -0400

    FormatMS.ts to display time on ui

---
+++ lib/formatMilliseconds.ts
@@ -0,0 +1,7 @@
+export function formatMS(milliseconds: number) {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const h = Math.floor(totalSeconds / 3600);
+  const m = Math.floor((totalSeconds % 3600) / 60);
+  const s = totalSeconds % 60;
+  return [h, m, s].map((n) => String(n).padStart(2, "0")).join(":");
+}
